6c96e37427c14c4eede8598cf2ceeb323ede660a
takaya-kinjo <beating428@gmail.com>
2021-05-14 15:00:55 +0900

    Revert "コンフリクトの訂正"
    This reverts commit 1bd5736d64bb530f2ab7b4237fcf3426d8ada115.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -45,6 +45,7 @@ class CalendarsController < ApplicationController
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: wdays[wday_num]  }
 
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, :plans => today_plans}
 
       @week_days.push(days)
     end
